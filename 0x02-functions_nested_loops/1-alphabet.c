03f7e651a5f3ca1ac32b9e88303ae4c38654549f
AminoS-04 <felhiamine0@gmail.com>
2023-03-21 19:12:40 -0700

    1. I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Main - prints all alphabet in lowercase
+ *
+ * print_alphabet: this function prints the alphabet in lowercase
+ * followed by a new line
+ */
+
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
