96f110decb348edcacb1aad488ec63143b113f66
AminoS-04 <felhiamine0@gmail.com>
2023-04-04 09:04:31 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-main.c
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include "string_functions.h"
+
 
 /**
  * main - check the code
---
+++ 0x07-pointers_arrays_strings/string_functions.h
@@ -0,0 +1,7 @@
+#ifndef STRING_FUNCTIONS_H
+#define STRING_FUNCTIONS_H
+
+void set_string(char **s1, char *s2);
+
+#endif /* STRING_FUNCTIONS_H */
+
