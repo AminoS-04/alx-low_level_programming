a22e8add7605b1f2fa6b40be15687ad43946ccd6
AminoS-04 <felhiamine0@gmail.com>
2023-04-11 18:02:33 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,13 +14,13 @@ char *_strdup(char *str)
 	char *array;
 
 	if (str == NULL)
-	        return (NULL);
+		return (NULL);
 
 	for (end = 0; end <= *str; end++)
 	{
 	}
 
-        end += 1;
+	end += 1;
 	array = malloc(sizeof(char) * end);
 
 	for (i = 0; i < end; i++)
