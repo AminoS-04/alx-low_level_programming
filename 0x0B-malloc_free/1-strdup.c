4d930ac5635ba400ef24af37f68dd8f29650f0fa
AminoS-04 <felhiamine0@gmail.com>
2023-04-11 18:19:11 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,55 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * _strdup - a function that returns a pointer to a newly allocated
- * space in memory, which contains a copy
- * of the string given as a parameter
- * @str: char
- * Return: 0
+ * _strlen - swap the values of two integers
+ * @s: pointer to a char
+ * Return: number of chars
+ */
+int _strlen(char *s)
+{
+	int acc = 0;
+
+	while (s[acc] != '\0')
+	{
+		acc++;
+	}
+	return (acc);
+}
+
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: Array of chars
+ *
+ * Return: NULL if string is NULL or error
+ * otherwise pointer to the duped arr
  */
 char *_strdup(char *str)
 {
-	int i, end;
-	char *array;
+	int i;
 
 	if (str == NULL)
-		return (NULL);
-
-	for (end = 0; end <= *str; end++)
 	{
-	}
-
-	end += 1;
-	array = malloc(sizeof(char) * end);
-
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
-
-	if (array == NULL)
 		return (NULL);
-	return (array);
+	}
+	else
+	{
+		int len = _strlen(str);
+		char *out = malloc(sizeof(char) * (len + 1));
+
+		if (out == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < len; i++)
+		{
+			*(out + i) = *(str + i);
+		}
+		*(out + i) = '\0';
+
+		return (out);
+	}
 }
